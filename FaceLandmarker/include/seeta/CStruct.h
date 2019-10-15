991e18a996d89e7d396c61fde2a0f35d26eea426
SeetaFace <53942815+seetafaceengine@users.noreply.github.com>
2019-10-15 15:47:24 +0800

    Update CStruct.h

--- FaceLandmarker/include/seeta/CStruct.h
+++ FaceLandmarker/include/seeta/CStruct.h
@@ -1,6 +1,18 @@
 #ifndef INC_SEETA_C_STRUCT_H
 #define INC_SEETA_C_STRUCT_H
 
+#ifdef _MSC_VER
+    #ifdef SEETA_EXPORTS
+        #define SEETA_API __declspec(dllexport)
+    #else
+        #define SEETA_API __declspec(dllimport)
+    #endif
+#else
+    #define SEETA_API __attribute__ ((visibility("default")))
+#endif
+
+#define SEETA_C_API extern "C" SEETA_API
+
 #define INCLUDED_SEETA_CSTRUCT
 
 #ifdef __cplusplus
