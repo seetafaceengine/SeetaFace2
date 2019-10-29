d16826335ca753341f28137418d2184e7a520e5a
KangLin <kl222@126.com>
2019-10-29 09:02:44 +0800

    FIX: Issues #77

--- FaceTracker/src/seeta/FaceTracker.cpp
+++ FaceTracker/src/seeta/FaceTracker.cpp
@@ -17,7 +17,7 @@ namespace seeta
 {
     namespace v2 {
         struct TrackedFace {
-            SeetaRect pos = {0, 0, 0, 0};
+            SeetaRect pos;
             int PID = 0;
             float conf = 0;
             int frame_no = 0;
